06d23ed9c534fb8845ef20d1bce1fe6f0fd4ffff
crazzyleucocyte <alstjr7696@naver.com>
2024-07-29 17:22:31 +0900

    .

--- jsp계정.sql
+++ jsp계정.sql
@@ -23,7 +23,19 @@ comment on column member.detailaddress is '상세주소';
 comment on column member.hobby is '취미';
 comment on column member.job is '직업';
 
+CREATE TABLE REPLY (
+    NO NUMBER PRIMARY KEY,
+    CONTENT VARCHAR2(400),
+    REF NUMBER,
+    NAME VARCHAR2(20),
+    REDATE DATE
+);
 
+CREATE SEQUENCE SEQ_REPLY NOCACHE;
+
+INSERT INTO REPLY VALUES(SEQ_REPLY.NEXTVAL, '와우!! 첫 댓글', 1, '김처음', '2024/07/01');
+INSERT INTO REPLY VALUES(SEQ_REPLY.NEXTVAL, '굉장하군요', 1, '박굉장', '2024/07/20');
+INSERT INTO REPLY VALUES(SEQ_REPLY.NEXTVAL, '멋져요', 1, '이멋짐', '2024/07/26');
 
 
 
